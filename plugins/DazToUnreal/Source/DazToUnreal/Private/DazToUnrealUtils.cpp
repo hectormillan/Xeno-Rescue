1c3b1919807eae120e504639d17f489985634339
hectormillan <hector.millan.83@hotmail.com>
2024-12-12 14:26:19 +0100

    mejoras en iluminacion e introduccion de plugn daz to unreal.

---
+++ plugins/DazToUnreal/Source/DazToUnreal/Private/DazToUnrealUtils.cpp
@@ -0,0 +1,105 @@
+#include "DazToUnrealUtils.h"
+#include "GenericPlatform/GenericPlatformFile.h"
+#include "Misc/Paths.h"
+#include "Engine/SkeletalMesh.h"
+
+
+#include "Engine/StaticMesh.h"
+#include "EditorFramework/AssetImportData.h"
+#include "Factories/FbxAssetImportData.h"
+
+FString FDazToUnrealUtils::SanitizeName(FString OriginalName)
+{
+	return OriginalName.Replace(TEXT(" "), TEXT(""))
+		.Replace(TEXT("("), TEXT("_"))
+		.Replace(TEXT(")"), TEXT("_"))
+		.Replace(TEXT("."), TEXT("_"))
+		.Replace(TEXT("&"), TEXT("_"))
+		.Replace(TEXT("!"), TEXT("_"))
+		.Replace(TEXT("*"), TEXT("_"))
+		.Replace(TEXT("<"), TEXT("_"))
+		.Replace(TEXT(">"), TEXT("_"))
+		.Replace(TEXT("?"), TEXT("_"))
+		.Replace(TEXT("\\"), TEXT("_"))
+		.Replace(TEXT(":"), TEXT("_"))
+		.Replace(TEXT("'"), TEXT("_"));
+}
+
+bool FDazToUnrealUtils::MakeDirectoryAndCheck(FString& Directory)
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (!FPaths::DirectoryExists(Directory))
+	{
+		PlatformFile.CreateDirectoryTree(*Directory);
+		if (!FPaths::DirectoryExists(Directory))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("DazToUnreal: ERROR: Unable to create directory tree: %s"), *Directory);
+			return false;
+		}
+	}
+	return true;
+}
+
+bool FDazToUnrealUtils::IsModelFacingX(UObject* MeshObject)
+{
+	if(USkeletalMesh* SkeletalMesh = Cast<USkeletalMesh>(MeshObject))
+	{
+#if ENGINE_MAJOR_VERSION == 4 && ENGINE_MINOR_VERSION < 27
+		if (UAssetImportData* AssetImportData = SkeletalMesh->AssetImportData)
+#else
+		if (UAssetImportData* AssetImportData = SkeletalMesh->GetAssetImportData())
+#endif
+		{
+			UFbxAssetImportData* FbxAssetImportData = Cast<UFbxAssetImportData>(AssetImportData);
+			if (FbxAssetImportData != nullptr && FbxAssetImportData->bForceFrontXAxis)
+			{
+				return true;
+			}
+		}
+	}
+	if (UStaticMesh* StaticMesh = Cast<UStaticMesh>(MeshObject))
+	{
+		if (UAssetImportData* AssetImportData = StaticMesh->AssetImportData)
+		{
+			UFbxAssetImportData* FbxAssetImportData = Cast<UFbxAssetImportData>(AssetImportData);
+			if (FbxAssetImportData != nullptr && FbxAssetImportData->bForceFrontXAxis)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+FString FDazToUnrealUtils::GetDTUPathForModel(FSoftObjectPath MeshObjectPath)
+{
+	if (USkeletalMesh* SkeletalMesh = Cast<USkeletalMesh>(MeshObjectPath.TryLoad()))
+	{
+#if ENGINE_MAJOR_VERSION == 4 && ENGINE_MINOR_VERSION < 27
+		if (UAssetImportData* AssetImportData = SkeletalMesh->AssetImportData)
+#else
+		if (UAssetImportData* AssetImportData = SkeletalMesh->GetAssetImportData())
+#endif
+		{
+			if (UFbxAssetImportData* FbxAssetImportData = Cast<UFbxAssetImportData>(AssetImportData))
+			{
+				for (FAssetImportInfo::FSourceFile SourceFile : FbxAssetImportData->GetSourceData().SourceFiles)
+				{
+					FString SourceFilePath = SourceFile.RelativeFilename;
+					TArray<FString> LikelyPaths;
+					LikelyPaths.Add(FPaths::ChangeExtension(SourceFilePath, TEXT("dtu")));
+					LikelyPaths.Add(FPaths::GetPath(SourceFilePath) + TEXT("/../") + FPaths::ChangeExtension(FPaths::GetCleanFilename(SourceFilePath), TEXT("dtu")));
+					for (FString PossiblePath : LikelyPaths)
+					{
+						if (FPaths::FileExists(PossiblePath))
+						{
+							return PossiblePath;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return FString();
+}
---
+++ plugins/DazToUnreal/Source/DazToUnreal/Public/DazToUnrealUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+class FDazToUnrealUtils
+{
+public:
+	static FString SanitizeName(FString OriginalName);
+	static bool MakeDirectoryAndCheck(FString& Directory);
+	static bool IsModelFacingX(UObject* MeshObject);
+	static FString GetDTUPathForModel(FSoftObjectPath MeshObjectPath);
+};
